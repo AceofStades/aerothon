a4402b509884b1888bf4e1fe32dc651546b192e3
AceofStades <rahulpbokade@gmail.com>
2026-04-02 00:48:39 +0530

    qr/qr: Add qreader

--- qr/qr.py
+++ qr/qr.py
@@ -1,13 +1,21 @@
 import cv2 as cv
+from qreader import QReader
 
 cap = cv.VideoCapture(0)
+qreader = QReader()
 ret = True
+count = 0
 while ret:
     ret, frame = cap.read()
     cv.imshow("webcam", frame)
 
     if cv.waitKey(1) & 0xFF == ord("q"):
         break
+
+    if count % 60 == 0:
+        text = qreader.detect_and_decode(frame)
+        print(text)
+    count += 1
 
 cap.release()
 cv.destroyAllWindows()
